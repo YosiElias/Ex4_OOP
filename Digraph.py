e5df97a30ebc12be17e5e6be416f8d4bb399d162
YosiElias <yosielias1313@gmail.com>
2022-01-02 04:10:26 +0200

    all work

---
+++ Digraph.py
@@ -0,0 +1,263 @@
+from random import random
+import random
+
+
+
+
+###############################################################################
+#                               class DiGraph                                    #
+###############################################################################
+import Node
+
+
+class DiGraph():
+
+    def __init__(self):
+        self._Nodes = {}
+        self._Edges = {}
+        self.edges_from = {}
+        self.edges_to = {}
+        self.num_of_edges = 0
+        self.MC = 0
+
+    def __repr__(self):
+        # ans = 'Nodes: '
+        # for n in self._Nodes.values():
+        #     ans = ans + str(n)+', '
+        # ans = ans + '\nEdges: '
+        # for e in self._Edges.values():
+        #     ans = ans + str(e)+', '
+        return '|V|={}, |E|={}'.format(len(self._Nodes), self.num_of_edges)
+
+
+    def getNode(self, node_id: int) -> Node:
+        return self._Nodes.get(node_id)
+
+    def getN(self):
+        return self._Nodes
+
+    def getEdge(self, id1, id2):
+        try:
+            return self._Edges.get(str(id1) + "-" + str(id2))
+        except:
+            return None
+
+    def get_all_v(self) -> dict:
+        return self._Nodes
+
+    def all_in_edges_of_node(self, id1: int) -> dict:
+        """return a dictionary of all the nodes connected to (into) node_id ,
+        each node is represented using a pair (other_node_id, weight)
+        """
+        return self.edges_to[id1]
+
+
+    def all_out_edges_of_node(self, id1: int) -> dict:
+        """return a dictionary of all the nodes connected from node_id , each node is represented using a pair
+        (other_node_id, weight)
+       """
+        return self.edges_from[id1]
+
+
+    def v_size(self) -> int:
+        return len(self._Nodes)
+
+    def e_size(self) -> int:
+        return self.num_of_edges
+
+    def get_mc(self) -> int:
+        return  self.MC
+
+    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        try:
+            if ((id1)) in self._Nodes and ((id2)) in self._Nodes and not ((id2)) in self.edges_from[id1]:
+                s1 = str(id1)
+                s2 = str(id2)
+                self._Edges[str(s1 + "-" + s2)] = Edge(id1, id2, weight)
+                self.edges_from[id1][id2] = weight
+                self._Nodes.get(id1).set_out(1)
+                self.edges_to[id2][id1] = weight
+                self._Nodes.get(id2).set_in(1)
+                self.num_of_edges = self.num_of_edges + 1
+                self.MC = self.MC + 1
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        try:
+            if not (node_id) in self._Nodes.keys():
+                self._Nodes[(node_id)] = Node(node_id, pos)
+                self.edges_from[node_id] = {}
+                self.edges_to[node_id] = {}
+                self.MC = self.MC + 1
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def getNeighboursDict(self, key: int) -> dict:
+        return self.edges_from[key]
+
+    def remove_node(self, node_id: int) -> bool:
+        try:
+            if (node_id) in self._Nodes.keys():
+                for id_dest in self.edges_from[(node_id)].keys():
+                    del self.edges_to[(id_dest)][(node_id)] # delete the edge from dict of 'edges_to'
+                for id_src in self.edges_to[(node_id)].keys():
+                    del self.edges_from[(id_src)][(node_id)] # delete the edge from dict of 'edges_to'
+                del self.edges_from[(node_id)]# delete the dict of edges from dict of 'edges_from'
+                del self.edges_to[(node_id)]# delete the dict of edges of 'edges_to'
+                del self._Nodes[(node_id)]
+                return True
+            else:
+                return False
+        except:
+            return False
+
+
+    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        try:
+            if (node_id2) in self.edges_from[(node_id1)]:
+                s1 = str(node_id1)
+                s2 = str(node_id2)
+                del self._Edges[str(s1 + "-" + s2)]
+                del self.edges_from[(node_id1)][(node_id2)]
+                self._Nodes.get(node_id1).set_out(-1)
+                del self.edges_to[(node_id2)][(node_id1)]
+                self._Nodes.get(node_id2).set_in(-1)
+                self.num_of_edges =  self.num_of_edges -1
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def getN(self):
+        return self._Nodes
+
+
+
+
+
+###############################################################################
+#                               class Edge                                    #
+###############################################################################
+
+
+class Edge:
+
+    def __init__(self, src, dest, weight):
+        self._src = src
+        self._dest = dest
+        self._weight = weight
+        self._color = (255, 52, 179)
+
+
+    def __repr__(self):
+        return '('+str(self._src)+'->'+str(self._dest)+', w:'+str(self._weight)+')'
+
+    def get_src(self):
+        return self._src
+
+    def get_dest(self):
+        return self._dest
+
+    def get_weight(self):
+        return self._weight
+
+    def get_color(self):
+        return self._color
+
+    def set_color(self, color:tuple):
+        self._color = color
+
+
+
+###############################################################################
+#                               class Node                                    #
+###############################################################################
+
+class Node:
+
+    def __init__(self, id, pos:tuple=None):
+        self._color = (245,255,250)
+        self._key = id
+        self._tag = -1
+        self._info = float('inf')
+        self._visited = False
+        self._maxDist = 1.7976931348623157E308
+        if pos == None:
+            self.set_location_random()
+        else:
+            if len(pos)==2:
+                pos_new = float(pos[0]), float(pos[1])
+            if len(pos)==3:
+                pos_new = float(pos[0]), float(pos[1]), float(pos[2])
+            self._location=pos_new
+        self._edges_out = 0
+        self._edges_in = 0
+
+    def __repr__(self):
+        return '{}: |edges_out| {} |edges in| {}'.format(self._key, self._edges_out, self._edges_in)
+
+    def get_out(self):
+        return self._edges_out
+
+    def set_out(self, n:int=0):
+        self._edges_out = self._edges_out +n
+
+    def get_in(self):
+        return self._edges_in
+
+    def set_in(self, n:int=0):
+        self._edges_in = self._edges_in + n
+
+    def get_id(self):
+        return self._key
+
+    def get_tag(self):
+        return self._tag
+
+    def get_info(self):
+        return self._info
+
+    def set_info(self,s):
+        self._info = s
+
+    def set_tag(self,s):
+        self._tag = s
+
+    def get_visit(self):
+        return self._visited
+
+    def set_visit(self, b:bool):
+        self._visited = b
+
+    def get_maxDist(self):
+        return self._maxDist
+
+    def set_maxDist(self, n):
+        self._maxDist = n
+
+    def get_color(self):
+        return self._color
+
+    def set_color(self, color: tuple):
+        self._color = color
+
+    def get_location(self) -> tuple:
+        return self._location
+
+    def set_location_random(self):
+        (x, y) = random.uniform(0, 100), random.uniform(0, 100)
+        self._location = (x, y)
+
+
+
+
+if __name__ == '__main__':
+    g = DiGraph()
---
+++ GraphAlgo.py
@@ -0,0 +1,254 @@
+import json
+import queue
+
+# from sqlalchemy import null
+from heapq import heappush, heappop  # Todo: !
+from itertools import count
+from queue import PriorityQueue
+
+
+# from Egde import Edge
+from Digraph import *
+# from Node import Node
+from collections import deque
+from decimal import Decimal
+from typing import List
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+
+class GraphAlgo():
+
+    def __init__(self):
+        # self._graph = self.load_from_json(path)
+        self._graph = DiGraph()
+        self.INF = float('inf')
+
+    def get_graph(self):
+        """
+        :return: the directed graph on which the algorithm works on.
+        """
+        return self._graph
+
+
+    # def load_from_json(self, file_name: str) -> bool:
+    #     """
+    #    Loads a graph from a json file.
+    #    @param file_name: The path to the json file
+    #    @returns True if the loading was successful, False o.w.
+    #    """
+    #     try:
+    #         if g == None:
+    #             return False
+    #         else:
+    #             self._graph = g
+    #             return True
+    #     except:
+    #         return False
+
+
+    def save_to_json(self, file_name: str) -> bool:
+        """
+        Saves the graph in JSON format to a file
+        @param file_name: The path to the out file
+        @return: True if the save was successful, False o.w.
+        """
+        try:
+            with open(file_name, 'w') as f:
+                dict_to_save = {}
+                dict_to_save["Edges"] = self._graph._Edges
+                for node in self._graph.getN().values():    # convert 'info' to str for legal json file
+                    if node.get_info() == float('inf'):
+                        node.set_info(str(node.get_info()))
+                dict_to_save["Nodes"] = self._graph.getN()
+                json.dump(dict_to_save, indent=2, fp=f, default=lambda a: a.__dict__)
+                for node in self._graph.getN().values():  # convert 'info' back to float for the next use of this data
+                    node.set_info(float(node.get_info()))
+            return True
+        except:
+            return False
+
+
+    def shortest_path(self, id1: int, id2: int):
+        """
+         Returns the shortest path from node id1 to node id2 using Dijkstra's Algorithm
+        :param id1: The start node id
+        :param id2: The end node id
+        :return: The distance of the path, a list of the nodes ids that the path goes through
+        """
+        curr = self._graph.getNode((id1))
+        dest = self._graph.getNode((id2))
+        if curr is None or dest is None:
+            return float(self.INF), []
+        n_list = self._graph.getN()
+        GraphAlgo.reset(self, n_list)
+        curr.set_info(0)
+        push = heappush # Todo: !
+        pop = heappop
+        c = count()
+        heap = []
+        push(heap, (0, next(c), curr))
+        # pq = PriorityQueue()
+        # pq.put((0,curr))
+        curr.set_visit(True)
+
+        # while not pq.empty():
+        while heap:
+            # (dist, cur_n) = pq.get()
+            (dist, _, cur_n) = pop(heap)
+            cur_key = cur_n.get_id()
+            dic = DiGraph.getNeighboursDict(self._graph, cur_n.get_id())
+            cur_n.set_visit(True)
+            for neighbour in dic.keys():
+                i = neighbour
+                node = self._graph.getNode(i)
+                if Node is not None and node.get_visit() is False:
+                # if Node is not None:
+                    edge = self._graph.getEdge(str(cur_key), str(node.get_id()))
+                    new_tag = edge.get_weight() + cur_n.get_info()
+                    if (new_tag < self.INF and node.get_info() == self.INF) or node.get_info() > new_tag:
+                        node.set_tag(cur_key)
+                        node.set_info(new_tag)
+                        # if node.get_visit() is False:
+                        # pq.put((new_tag, node))
+                        # node.set_visit(True)
+                        push(heap, (new_tag, next(c), node))
+
+        if dest.get_info() is self.INF:
+            return float('inf'), []
+        path_list = GraphAlgo.find_path(self, id2, id1)
+        return dest.get_info(), path_list
+
+
+    def find_path(self, dest: int, src: int):
+        curr = self._graph.getNode((dest))
+        stack = deque()
+        while curr.get_id() is not src:
+            # i = curr.get_id()
+            stack.append(curr.get_id())
+            if curr.get_tag is self.INF:
+                return []
+            if curr is not None:
+                curr = self._graph.getNode((curr.get_tag()))
+                if curr is None:
+                    break
+        if curr is not None:
+            stack.append(curr.get_id())
+
+        counter = GraphAlgo.index_num(self, stack)
+        list = []
+        for i in range(counter):
+            temp_n = stack.pop()
+            list.append(temp_n)    #Todo: yossi change from: 'list.append(temp_n)'
+        return list
+
+
+    def index_num(self, stack: deque):
+        cou = 0
+        for i in stack:
+            cou = cou+1
+        return cou
+
+    def reset(self, node_list: dict):
+        for id in node_list:
+            node: Node = node_list.get((id))
+            node.set_tag(-1)
+            node.set_info(self.INF)
+            node.set_visit(False)
+
+    def TSP(self, node_lst: List[int]) -> (List[int], float):
+        """
+        Finds the shortest path that visits all the nodes in the list
+        :param node_lst: A list of nodes id's
+        :return: A list of the nodes id's in the path, and the overall distance
+        """
+        finalCost = 0
+        path = []
+        path.append(node_lst[0])
+        tempPath = []
+
+        for j in range(len(node_lst)):  # loop on all the cities and search path from the city that my path get to NodeData
+            src_id = path[len(path) - 1]  # take the last node in the path
+            minCost = float('inf')
+            # reset distances from 'src' node:
+            if (j == 0 and len(node_lst) >= 2):  # if first loop AND have >= 2 node in cities:
+                comparTo_id = node_lst[1]
+            else:
+                comparTo_id = node_lst[0]
+            short_path = self.shortest_path(src_id, comparTo_id)
+
+            for i in range(len(node_lst)):
+                dest: Node = self._graph.getNode(node_lst[i])
+                if (not dest.get_id() == src_id and not dest.get_id() in path):
+                    # not check path from node to itself AND search only for nodes that not in the path yet
+                    cost = dest.get_info()
+                    if (cost < minCost):
+                        minPath = self.shortest_path(src_id, dest.get_id())[
+                            1]  # Todo: check that the second play of 'shortest_path' is not arm the 'info' of dist of all nodes
+                        del minPath[0]
+                        minCost = cost
+                        tempPath = minPath
+            for i in range(len(tempPath)):
+                # add the chosen path to cost and to path
+                path.append(tempPath[i])
+            tempPath = []
+            if not minCost == float('inf'):
+                finalCost = finalCost + minCost
+
+        return (path, finalCost)
+
+    def centerPoint(self) -> (int, float):
+        """
+        Finds the node that has the shortest distance to it's farthest node.
+        :return: The nodes id, min-maximum distance
+        """
+        num, l = self.update_maxDist()
+        if l == float('inf'):
+            return None, float('inf')
+        # now find the minimum max dist of all nodes
+        n_list = self._graph.getN()
+        min_node = None
+        max_val = self.INF
+        for n in n_list:
+            cur_n = self._graph.getNode((n))
+            dist = cur_n.get_maxDist()
+            if dist < max_val:
+                max_val = dist
+                min_node = cur_n
+        return min_node.get_id(), min_node.get_maxDist()
+
+    def update_maxDist(self):
+        n_list = self._graph.getN()
+        # update the largest distance to every node - to be distance from every node to the his furthest other node
+        for no in n_list:
+            # if no == str(len(n_list) - 1):
+            if no == str(len(n_list) -1):
+                max_dist = -float('inf')
+                cur_n = self._graph.getNode((no))
+                n = int(no)
+                self.shortest_path(n, n - 1)
+            else:
+                max_dist = -float('inf')
+                cur_n = self._graph.getNode((no))
+                n = int(no)
+                self.shortest_path(n, n + 1)
+            for node in n_list:
+                if no is not node:
+                    temp_n = self._graph.getNode((node))
+                    dist = temp_n.get_info()
+                    if dist is not self.INF:
+                        if dist > max_dist:
+                            max_dist = dist
+                    else:
+                        # if the graph is not connected
+                        return None, float('inf')
+            cur_n.set_maxDist(max_dist)
+        return 1,[]
+
+
+    def __str__(self):
+        return self._graph
